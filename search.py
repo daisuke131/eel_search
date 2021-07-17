692bbad74bd20f633a0b8ffdb12a8b656f491bcb
daisuke <dada.00131@gmail.com>
2021-07-17 16:14:10 +0900

    windows用に修正

--- search.py
+++ search.py
@@ -1,7 +1,9 @@
+import os
+
 import eel
 
-READ_CSV_PATH = "./csv/character.csv"
-WRITE_CSV_PATH = "./csv/{csv_name}.csv"
+READ_CSV_PATH = os.path.join(os.getcwd(), "csv\\character.csv")
+WRITE_CSV_PATH = os.path.join(os.getcwd(), "csv\\{csv_name}.csv")
 
 
 def search(csv_name: str, search_word: str):
@@ -9,23 +11,22 @@ def search(csv_name: str, search_word: str):
     characters = read_csv()
     if search_word in characters:
         print(f"{search_word}が見つかりました。")
-        eel.output_logs(f"{search_word}が見つかりました。\n")
+        eel.output_logs(f"{search_word}が見つかりました。")
     else:
         characters.append(search_word)
         print("リストに存在しなかったので追加しました。")
-        eel.output_logs("リストに存在しなかったので追加しました。\n")
-
-    # CSV書き込み
-    write_csv(characters, csv_name)
+        eel.output_logs("リストに存在しなかったので追加しました。")
+        # CSV書き込み
+        write_csv(characters, csv_name)
 
 
 def read_csv():
-    with open(READ_CSV_PATH) as f:
+    with open(READ_CSV_PATH, encoding="utf_8_sig") as f:
         read_csv_str = f.read()
         return read_csv_str.split(",")
 
 
 def write_csv(characters: str, csv_name: str):
     write_csv_path = WRITE_CSV_PATH.format(csv_name=csv_name)
-    with open(write_csv_path, mode="w") as f:
+    with open(write_csv_path, mode="w", encoding="utf_8_sig") as f:
         f.write(",".join(characters))
